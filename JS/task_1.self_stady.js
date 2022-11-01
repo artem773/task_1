339fc8df8da9bc620b461a448ee7f019fdade157
Artem <artem1407@gmail.com>
2022-11-01 21:22:32 +0200

    add 2d part to task -- logicheskie principi i cikli

--- JS/task_1.self_stady.js
+++ JS/task_1.self_stady.js
@@ -23,6 +23,24 @@
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+/* Задание на урок 2:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
 "use strict";
 
 
@@ -36,13 +54,28 @@ let personalMovieDB = {
     privat: false,
 };
 
-const questionOne = prompt('Один из последних просмотренных фильмов ?',''),
-      questionOneEstimate = +prompt('На сколько его оценіте ?',''),
-      questionTwo = prompt('Один из последних просмотренных фильмов ?',''),
-      questionTwoEstimate = +prompt('На сколько его оценіте ?','');
 
+for(let i = 0; i < 2; i++){
+    const questionOne = prompt('Один из последних просмотренных фильмов ?',''),
+          questionTwo = +prompt('На сколько его оценіте ?','');
+    
+    if(questionOne != '' && questionTwo != '' & questionOne != null && questionTwo != null && questionOne.length < 50){
+        personalMovieDB.movies[questionOne] = questionTwo;
+        console.log('DONE!');
+    }else{
+        i--;
+        console.log('ERROR!');
+    }
+}
 
-personalMovieDB.movies[questionOne] = questionOneEstimate;
-personalMovieDB.movies[questionTwo] = questionTwoEstimate;
+if(personalMovieDB.count < 10){
+    console.log("Просмотрено довольно мало фильмов");
+}else if( personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log("Вы классический зритель");    
+}else if(personalMovieDB.count >= 30){
+    console.log("Вы киноман !");
+}else {
+    console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB); // необходимо написать вывод в консоль что бы даже в браузере у нас отображались введенные данные в консоли !!!!!!!!
