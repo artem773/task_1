f5e9e3682fae346f6d19f13d970ddeeefe47ab6e
Artem <artem1407@gmail.com>
2022-11-05 17:02:04 +0200

    the third practice task - done

--- JS/task_1.self_stady.js
+++ JS/task_1.self_stady.js
@@ -39,12 +39,34 @@
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+/* Задание на урок 3 :
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres */
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 "use strict";
 
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели ?','');
+let numberOfFilms; 
+
+function start(){
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели ?','');
+
+    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели ?','');
+    }
+}
+
+start();
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -55,27 +77,50 @@ let personalMovieDB = {
 };
 
 
-for(let i = 0; i < 2; i++){
-    const questionOne = prompt('Один из последних просмотренных фильмов ?',''),
-          questionTwo = +prompt('На сколько его оценіте ?','');
-    
-    if(questionOne != '' && questionTwo != '' & questionOne != null && questionTwo != null && questionOne.length < 50){
-        personalMovieDB.movies[questionOne] = questionTwo;
-        console.log('DONE!');
-    }else{
-        i--;
-        console.log('ERROR!');
+function rememberMyFilms(){
+    for(let i = 0; i < 2; i++){
+        const questionOne = prompt('Один из последних просмотренных фильмов ?',''),
+              questionTwo = +prompt('На сколько его оценіте ?','');
+        
+        if(questionOne != '' && questionTwo != '' & questionOne != null && questionTwo != null && questionOne.length < 50){
+            personalMovieDB.movies[questionOne] = questionTwo;
+            console.log('DONE!');
+        }else{
+            i--;
+            console.log('ERROR!');
+        }
     }
 }
 
-if(personalMovieDB.count < 10){
-    console.log("Просмотрено довольно мало фильмов");
-}else if( personalMovieDB.count >= 10 && personalMovieDB.count < 30){
-    console.log("Вы классический зритель");    
-}else if(personalMovieDB.count >= 30){
-    console.log("Вы киноман !");
-}else {
-    console.log("Произошла ошибка");
+rememberMyFilms();
+
+function detectPersonalLevel(){
+    if(personalMovieDB.count < 10){
+        console.log("Просмотрено довольно мало фильмов");
+    }else if( personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+        console.log("Вы классический зритель");    
+    }else if(personalMovieDB.count >= 30){
+        console.log("Вы киноман !");
+    }else {
+        console.log("Произошла ошибка");
+    }
 }
 
-console.log(personalMovieDB); // необходимо написать вывод в консоль что бы даже в браузере у нас отображались введенные данные в консоли !!!!!!!!
+detectPersonalLevel();
+
+function writeYourGenres(){
+    for(let i = 1; i <= 3; i++){
+        const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+        personalMovieDB.genres[i - 1] = genre;
+    }
+}
+
+writeYourGenres();
+
+function showMyDB(){
+    if(personalMovieDB.privat !== true){
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB();
